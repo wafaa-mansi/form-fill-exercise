4b1b92127189078f9e49dcf7101fec81b63d782d
wafaa-mansi <wafaa_mansi@icloud.com>
2026-09-21 17:54:00 +0300

    -- to fix main branch

--- Form.js
+++ Form.js
@@ -6,14 +6,17 @@ const userTable = document.getElementById("userTable");
 
 let users = JSON.parse(localStorage.getItem("users")) || [];
 
-debugger;
-// Checks if all fields are written 
+let isEditing = false;
+
+
+// Checks if all fields are written
 function checkForm() {
 
   if (
     firstName.value.trim() !== "" &&
     lastName.value.trim() !== "" &&
-    gender.value !== ""
+    gender.value !== "" &&
+    isEditing === false
   ) {
     saveBtn.disabled = false;
   }
@@ -29,41 +32,112 @@ firstName.addEventListener("input", checkForm);
 lastName.addEventListener("input", checkForm);
 gender.addEventListener("change", checkForm);
 
-// Save button
-saveBtn.addEventListener("click", function() {
 
-  const row = document.createElement("tr");
+// Display users
+function displayUsers() {
 
-  row.innerHTML = `
-    <td>${firstName.value}</td>
-    <td>${lastName.value}</td>
-    <td>${gender.value}</td>
-    <td>
-      <button class="delete-btn">Delete</button>
-    </td>
-  `;
+  userTable.innerHTML = "";
 
-  userTable.appendChild(row);
-  localStorage.setItem("users", JSON.stringify(users)); 
+  users.forEach(function(user, index) {
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.gender}</td>
+      <td>
+        <button class="edit-btn">Edit</button>
+        <button class="delete-btn">Delete</button>
+      </td>
+    `;
 
 
-  // Delete button
-  const deleteBtn = row.querySelector(".delete-btn");
+    // Edit button
+    const editBtn = row.querySelector(".edit-btn");
 
-  deleteBtn.addEventListener("click", function() {
-    row.remove();
+    editBtn.addEventListener("click", function() {
 
-    localStorage.removeItem("users");
+      if (editBtn.innerText === "Edit") {
+
+        firstName.value = user.firstName;
+        lastName.value = user.lastName;
+        gender.value = user.gender;
+
+        editBtn.innerText = "Save";
+
+        isEditing = true;
+
+        saveBtn.disabled = true;
+      }
+
+      else {
+
+        users[index].firstName = firstName.value;
+        users[index].lastName = lastName.value;
+        users[index].gender = gender.value;
+
+        localStorage.setItem("users", JSON.stringify(users));
+
+        isEditing = false;
+
+        firstName.value = "";
+        lastName.value = "";
+        gender.value = "";
+
+        displayUsers();
+
+        checkForm();
+      }
+
+    });
+
+
+    // Delete button
+    const deleteBtn = row.querySelector(".delete-btn");
+
+    deleteBtn.addEventListener("click", function() {
+
+      users.splice(index, 1);
+
+      localStorage.setItem("users", JSON.stringify(users));
+
+      displayUsers();
+
+    });
+
+
+    userTable.appendChild(row);
+
   });
 
+}
 
-  // Clear fields
+
+saveBtn.addEventListener("click", function() {
+
+  const user = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    gender: gender.value
+  };
+
+  users.push(user);
+
+  localStorage.setItem("users", JSON.stringify(users));
+
+  displayUsers();
+
+
+  
   firstName.value = "";
   lastName.value = "";
   gender.value = "";
 
 
-  // Disable save button again
   checkForm();
 
 });
+
+
+displayUsers();
